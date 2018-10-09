c12821e664a611a4892cdc407f711ea4b2a0dade
cindelint <cindylee776@gmail.com>
2018-10-09 10:30:23 -0700

    made basic NormalParticle

--- build/source/starfield.java
+++ build/source/starfield.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class starfield extends PApplet {
+
 Particle[] bobert;
 
-void setup() {
-  size(500,500);
+public void setup() {
+  
   noStroke();
   bobert = new Particle[100];
   for (int i=0; i<bobert.length; i++) {
@@ -9,7 +25,7 @@ void setup() {
   }
 }
 
-void draw() {
+public void draw() {
   background(0);
   for (int i=0; i<bobert.length; i++) {
     bobert[i].show();
@@ -23,7 +39,7 @@ void draw() {
 
 class NormalParticle implements Particle{
   public double xPos, yPos;
-  public color pColor;
+  public int pColor;
   public double angle, speed;
   public int startX, startY;
   NormalParticle(int x, int y) {
@@ -59,4 +75,14 @@ class OddballParticle implements Particle { //uses an interface
 
 class JumboParticle implements Particle{ //uses inheritance
 
+}
+  public void settings() {  size(500,500); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
